3a5000ef5f3a4a15da484d979c83ada19d19217b
rune.laenen <rune.laenen@intracto.com>
2020-11-02 12:31:46 +0100

    Code review additions

--- src/Resources/app/administration/src/module/blog-module/page/sas-blog-detail/index.js
+++ src/Resources/app/administration/src/module/blog-module/page/sas-blog-detail/index.js
@@ -152,7 +152,6 @@ Component.register('sas-blog-detail', {
                     },
                     image: SimpleImage,
                     delimiter: Delimiter,
-                    raw: RawTool,
                     table: {
                         class: Table,
                         inlineToolbar: true,
